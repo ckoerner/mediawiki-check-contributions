0a19eb685a950c6502b172a42afab492e01872ea
ckoerner <nobelx@gmail.com>
2015-08-07 15:17:13 -0500

    Initial load

---
+++ README.md
@@ -0,0 +1,17 @@
+#Who Contributes?
+
+This is a simple Python script that helps determine who edited a given
+wiki page. Often I get asked by folks "Who has edited <group of pages>
+on our wiki?" I get a list of pages (either by gathering them from a
+Category or from a quervy via the ReplaceText extension. For example,
+someone asked if I could find out who has contributed to all pages that
+mention a particular phrase. I'd run the string through ReplaceText, and
+put the list of titles into this script.
+
+
+page-titles.txt Add a list of all pages you want to check. Each page
+title should be on it’s own line.
+
+output.txt
+
+Your output will go here. Re-running the script will overwrite this file!
---
+++ whoContributes.py
@@ -0,0 +1,37 @@
+#title			:gitMediawiki.sh
+#description	:run through a list of wiki pages (listed in a text file) and gather the list of contributors
+#author			:Chris Koerner & Paul Boal
+#date			:20150907
+#version		:0.1
+#usage			:python whoContributes.py
+#notes			:Make sure you change the script to point to your wiki's URL!
+#prerequisites	:This script also assumes you have Python installed :)
+
+import json
+import urllib
+ 
+ 
+f = open('page-titles.txt', 'r')
+w = open('output.txt', 'w')
+for line in f.readlines():
+    id = line.strip('\n')
+    url = "https://yourwiki.address.here/w/api.php?action=query&titles={0}&prop=contributors&format=json&rawcontinue=".format(id)
+    urlobj = urllib.urlopen(url)
+ 
+    sock = urllib.urlopen(url)
+    content = sock.read()
+    sock.close()
+ 
+    contentObj = json.loads(content)
+    query = contentObj['query']
+    if query.has_key('pages'):
+        pages = query['pages']
+ 
+        for (pid, page) in pages.iteritems():
+            title = page['title']
+            if page.has_key('contributors'):
+                contributors = page['contributors']
+                for c in contributors:
+                    out = title + '|' + c['name'] + '\n'
+                    print out
+                    w.write(out)
